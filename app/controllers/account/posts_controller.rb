95b6605e2f892da25a3e59a50f97fbd7a4fa5eca
Yung Yuan <leonychuang@gmail.com>
2016-07-25 00:12:16 +0800

    before edit/destroy

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -4,18 +4,4 @@ class Account::PostsController < ApplicationController
   def index
     @posts = current_user.posts
   end
-
-  def update
-    if @group.update(group_params)
-      redirect_to groups_path, notice: "修改成功"
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @posts.destroy
-    #redirect_to groups_path, alert: "討論組已刪除"
-  end
-
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,9 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+  end
+
   private
 
   def post_params
